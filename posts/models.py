56466ad4f01d42ed22257d0f628ef1e08f017332
Dmytriy Myk <porosya@gmail.com>
2019-08-30 00:01:44 +0300

    another try

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,18 @@
 from django.conf import settings
+from django.contrib.contenttypes.fields import (GenericForeignKey,
+                                                GenericRelation)
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from users.models import User
+
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name='likes',
+                             on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
 
 class Post(models.Model):
@@ -11,11 +23,11 @@ class Post(models.Model):
     text = models.TextField()
     visible = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    likes = models.ManyToManyField(User, related_name="likes")
+    likes = GenericRelation(Like)
 
     def __str__(self):
         return self.title
 
-    # @property
-    # def total_likes(self):
-    #     return self.likes.count()
+    @property
+    def total_likes(self):
+        return self.likes.count()
